380a0ebe8fc132b041bc3cc5224912276111a8c7
SaxsCode <maxsax.sch@gmail.com>
2025-05-29 15:21:40 +0200

    feat: add status messages to ftp upload

--- lua/windows-sync/init.lua
+++ lua/windows-sync/init.lua
@@ -41,6 +41,7 @@ local function upload_file(filepath, ftp_config)
 	-- Run the FTP command
 	local cmd = string.format('ftp -s:"%s"', script_path)
 	os.execute(cmd)
+	print("File uploaded to remote server: " .. filepath)
 
 	-- Clean up
 	os.remove(script_path)
